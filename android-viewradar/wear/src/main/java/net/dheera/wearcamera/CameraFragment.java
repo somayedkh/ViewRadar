ad0b86b0f63c7c7722c05655925a666b2809a629
Donavan Asmawidjaja <donavan.asmawidjaja@outlook.com>
2017-04-05 11:23:30 +0200

    [Android] Android Project with phone app and wear app.

---
+++ android-viewradar/wear/src/main/java/net/dheera/wearcamera/CameraFragment.java
@@ -0,0 +1,95 @@
+package net.dheera.wearcamera;
+
+import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import static android.content.Context.VIBRATOR_SERVICE;
+
+public class CameraFragment extends Fragment {
+    private View.OnClickListener mOnClickListener;
+
+    public ImageView cameraPreview = null;
+    public ImageView cameraResult;
+    public TextView cameraTime;
+    public LinearLayout overlayLayout;
+    public TextView overlayTitle;
+    public TextView overlayClose;
+
+    public Boolean showWarning = false;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View contentView = inflater.inflate(R.layout.camera, container, false);
+        cameraPreview = (ImageView) contentView.findViewById(R.id.camera_preview);
+        cameraTime = (TextView) contentView.findViewById(R.id.camera_time);
+        cameraResult = (ImageView) contentView.findViewById(R.id.camera_result);
+        overlayLayout = (LinearLayout) contentView.findViewById(R.id.overlay_layout);
+        overlayTitle = (TextView) contentView.findViewById(R.id.overlay_title);
+        overlayClose = (TextView) contentView.findViewById(R.id.overlay_close);
+        cameraPreview.setOnClickListener(mOnClickListener);
+
+        SharedPreferences preferences = getActivity().getPreferences(0);
+        final Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (getActivity() != null) {
+                    if (((MainActivity) getActivity()).isFaceDetected() && !showWarning) {
+                        showWarning = true;
+                        setWarningScreen();
+                    }
+                }
+                h.postDelayed(this, 10);
+            }
+        }, 10);
+//        if (preferences.getBoolean("tip0_hide", false) == false) {
+//            mImageView_tip = (TextView) contentView.findViewById(R.id.tip);
+//            //mImageView_tip.setImageResource(R.drawable.tip_0);
+//            mImageView_tip.setVisibility(View.VISIBLE);
+//            mImageView_tip.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    mImageView_tip.setVisibility(View.GONE);
+//                    SharedPreferences preferences = getActivity().getPreferences(0);
+//                    SharedPreferences.Editor editor = preferences.edit();
+//                    editor.putBoolean("tip0_hide", true);
+//                    editor.commit();
+//                }
+//            });
+//        }
+
+        return contentView;
+    }
+
+    public void setWarningScreen() {
+        overlayTitle.setText("Pas op!");
+        overlayTitle.setVisibility(View.VISIBLE);
+        Vibrator vibrator = (Vibrator) getActivity().getSystemService(VIBRATOR_SERVICE);
+        long[] vibrationPattern = {0, 1000, 50, 1000};
+        //-1 - don't repeat
+        final int indexInPatternToRepeat = -1;
+        vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
+        ((MainActivity) getActivity()).setFaceDetected(false);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                overlayTitle.setVisibility(View.GONE);
+                overlayClose.setVisibility(View.VISIBLE);
+                showWarning = false;
+            }
+        }, 10000);
+    }
+
+    public void setOnClickListener(View.OnClickListener listener) {
+        mOnClickListener = listener;
+    }
+}
---
+++ android-viewradar/wear/src/main/java/net/dheera/wearcamera/MenuAdapter.java
@@ -0,0 +1,73 @@
+package net.dheera.wearcamera;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.os.Handler;
+import android.support.wearable.view.FragmentGridPagerAdapter;
+import android.util.Log;
+import android.view.View;
+public class MenuAdapter extends FragmentGridPagerAdapter {
+
+    private final Context mContext;
+    private final Handler mHandler;
+    public final CameraFragment mCameraFragment;
+
+    public static final int MESSAGE_SNAP = 1;
+    public static final int MESSAGE_SWITCH = 2;
+
+    private static int currentCamera = 0;
+    int[] currentCameraText = { R.string.action_switch_0, R.string.action_switch_1 };
+    int[] currentCameraIcon = { R.drawable.action_switch_0, R.drawable.action_switch_1 };
+
+    public MenuAdapter(Context ctx, FragmentManager fm, Handler h) {
+        super(fm);
+        mContext = ctx;
+        mHandler = h;
+        mCameraFragment = new CameraFragment();
+        mCameraFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("blah", "clicked snap");
+                mHandler.obtainMessage(MESSAGE_SNAP).sendToTarget();
+            }
+        });
+    }
+
+    @Override
+    public int getColumnCount(int arg0) {
+        return 2;
+    }
+
+    @Override
+    public int getRowCount() {
+        return 1;
+    }
+
+    @Override
+    public Fragment getFragment(int rowNum, int colNum) {
+        Log.d("blah", String.format("getFragment(%d, %d)", rowNum, colNum));
+
+        if(colNum == 0) {
+            return mCameraFragment;
+        }
+
+        if(colNum == 1) {
+            final ActionFragment switchAction = ActionFragment.newInstance(currentCameraIcon[currentCamera], currentCameraText[currentCamera]);
+            switchAction.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("blah", "clicked switch");
+                    currentCamera = ( currentCamera + 1 ) % currentCameraText.length;
+                    switchAction.setTextRes(currentCameraText[currentCamera]);
+                    switchAction.setIconRes(currentCameraIcon[currentCamera]);
+                    mHandler.obtainMessage(MESSAGE_SWITCH, currentCamera, -1).sendToTarget();
+                }
+            });
+            return switchAction;
+        }
+
+        return null;
+    }
+
+}
